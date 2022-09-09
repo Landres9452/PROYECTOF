c45470ff5fc63d18bdee2878d192e0f019f033d2
Landres9452 <112189548+Landres9452@users.noreply.github.com>
2022-09-09 14:03:04 -0500

    Agregar archivos de proyecto.

---
+++ PROYECTOF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROYECTOF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
